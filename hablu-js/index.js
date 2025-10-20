3d3cb44500516226ec01a86dde8633711df41b67
ridwancodexr <ridwancodexr@gmail.com>
2025-10-20 20:38:37 +0600

    add

---
+++ hablu-js/index.js
@@ -0,0 +1,12 @@
+
+let userOneValue = prompt(" Number One Data ") ;
+let userTwoValue = prompt( " Number Two Data " ) ;
+
+userOneValue = parseInt(userOneValue) ;
+userTwoValue = parseInt(userTwoValue) ;
+
+
+const Result = userOneValue + userTwoValue ;
+
+
+document.write(  " Output Resut " + Result + " <br> " ) ;
