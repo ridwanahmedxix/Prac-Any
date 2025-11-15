9d67000fd1261638bcbf968ae59bc8c960a9b46a
ridwancodexr <ridwancodexr@gmail.com>
2025-11-15 07:16:44 +0600

    CurentTime add

--- JavaScriptProjects/pro-1/clock.js
+++ JavaScriptProjects/pro-1/clock.js
@@ -4,4 +4,18 @@ function myTime() {
     let hr,min = (myDate.getMinutes() < 10 ) ? "0" + myDate.getMinutes() : myDate.getMinutes(),
        sec = (myDate.getSeconds() < 10 ) ? "0" + myDate.getSeconds() : myDate.getSeconds() ,
        M = (myDate.getHours() >=12 ) ? "PM" :"AM" ;
+
+
+    if(myDate.getHours () == 0 ){
+        hr = 12 ;
+    }else if (myDate.getHours () > 12) {
+       hr = myDate.getHours() - 12 ;
+    }else{
+      hr = myDate.getHours() ;
+    }
+
+    let CurentTime = hr + ":" + min + ":" + sec + ":"
+
+
+
 }
