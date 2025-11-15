8dc04e59dbb8e155296ba4dd5146d3f97b1a2dff
ridwancodexr <ridwancodexr@gmail.com>
2025-11-15 06:58:01 +0600

    sec variable create

--- JavaScriptProjects/pro-1/clock.js
+++ JavaScriptProjects/pro-1/clock.js
@@ -2,4 +2,4 @@
 function myTime() {
     let myDate = new Date() ;
     let hr,min = (myDate.getMinutes() < 10 ) ? "0" + myDate.getMinutes() : myDate.getMinutes() ;
-}
+}        sec = (myDate.getSeconds() < 10 ) ? "0" + myDate.getSeconds() : myDate.getSeconds() ;
