292832bf35e5ceabda514c6f6f061882e458699d
ridwancodexr <ridwancodexr@gmail.com>
2025-11-16 11:05:21 +0600

    javascript code done

---
+++ ResponsiveNav/nav.js
@@ -0,0 +1,10 @@
+
+function showSidebar() {
+    const sideBar = document.querySelector(".sideBar")
+    sideBar.style.display = 'flex' ;
+}
+
+function hideShidebar() {
+    const sideBar = document.querySelector(".sideBar")
+    sideBar.style.display = 'none' ;
+}
