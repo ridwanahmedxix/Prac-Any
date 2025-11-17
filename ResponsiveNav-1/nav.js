a7c8a633df514548ac63a6a92b0b5b7cd2d3e87a
ridwancodexr <ridwancodexr@gmail.com>
2025-11-17 15:16:38 +0600

    Responsive nav create complete

--- ResponsiveNav-1/nav.js
+++ ResponsiveNav-1/nav.js
@@ -2,3 +2,7 @@ function showSidebar() {
  const sideBar =  document.querySelector(".sideBar")
     sideBar.style.display = "flex"
 }
+function hideSidebar() {
+ const sideBar =  document.querySelector(".sideBar")
+    sideBar.style.display = "none"
+}
