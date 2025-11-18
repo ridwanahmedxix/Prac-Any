1c14620af51db9ec4574b7aef8bc2039619f0409
ridwancodexr <ridwancodexr@gmail.com>
2025-11-18 18:57:20 +0600

    update

---
+++ JavaScriptProjects/pro-2/bgColor.js
@@ -0,0 +1,16 @@
+
+function bgColorRed() {
+document.body.style.backgroundColor = 'red'
+}
+
+function bgColorBlue() {
+document.body.style.backgroundColor = 'blue'
+}
+
+function bgColorGreen() {
+document.body.style.backgroundColor = 'green'
+}
+
+function bgColorYello() {
+document.body.style.backgroundColor = 'yellow'
+}
