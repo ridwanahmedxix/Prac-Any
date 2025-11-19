5753bede4c4efde6b0e266ceaa0f2e1c026aa57f
ridwancodexr <ridwancodexr@gmail.com>
2025-11-19 16:06:20 +0600

    auto color  button create

--- JavaScriptProjects/pro-3/about.js
+++ JavaScriptProjects/pro-3/about.js
@@ -1,5 +1,14 @@
 const body = document.querySelector("body") ;
 
 function setColor(cName) {
-    body.style.backgroundColor = cName ;
+    document.body.style.backgroundColor = cName ;
 }
+
+function randomC(){
+    const red = Math.round(Math.random()*256)
+    const green = Math.round(Math.random()*256)
+    const blue = Math.round(Math.random()*256)
+    const color = `rgb(${red} ,${green} ,${blue} )` ;
+    document.body.style.backgroundColor = color ;
+}
+
