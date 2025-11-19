309c9350c6fb5c587273a4aea2a9fb6b92de699c
ridwancodexr <ridwancodexr@gmail.com>
2025-11-19 16:16:48 +0600

    auto colorchange create

--- JavaScriptProjects/pro-3/about.js
+++ JavaScriptProjects/pro-3/about.js
@@ -1,14 +1,13 @@
 const body = document.querySelector("body") ;
-
-function setColor(cName) {
-    document.body.style.backgroundColor = cName ;
+function setColor (cName){
+  document.body.style.backgroundColor = cName ;
 }
 
 function randomC(){
     const red = Math.round(Math.random()*256)
     const green = Math.round(Math.random()*256)
     const blue = Math.round(Math.random()*256)
-    const color = `rgb(${red} ,${green} ,${blue} )` ;
-    document.body.style.backgroundColor = color ;
+    const color = ` rgb(${red} , ${green} , ${blue})` ;
+    body.style.backgroundColor = color
 }
 
