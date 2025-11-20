40dda55968da7ba68c8adead2153837c1a0f8915
ridwancodexr <ridwancodexr@gmail.com>
2025-11-20 11:11:24 +0600

    model collage website create done

---
+++ project-23/home.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+ const sideBar =  document.querySelector(".sideBar")
+    sideBar.style.display = "flex"
+}
+function hideSidebar() {
+ const sideBar =  document.querySelector(".sideBar")
+    sideBar.style.display = "none"
+}
